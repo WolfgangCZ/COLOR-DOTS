4864c122cd287729f913f3d9c007a2b58b9c7a33
Wolfgang_CZ <kralicek.roman@gmail.com>
2023-03-21 20:45:12 +0100

    added player, draw and update movement

--- main.cpp
+++ main.cpp
@@ -3,28 +3,34 @@
 #include <memory>
 //#include "dot.h"
 #include "raylib.h"
+#include "raymath.h"
 //#include "world.h"
 
 
 
 extern const std::size_t screen_w{800};
 extern const std::size_t screen_h{600};
-extern const std::size_t starting_dots {400};
+extern const std::size_t starting_dots {2};
+extern const std::size_t gravity_modifier {1};
+extern const float player_speed {1};
+extern const float friction {0.9};
 
 struct Dot
 {
     public:
         Dot() = default;
         Dot(float x, float y, float v_x, float v_y)
-            : m_x(x), m_y(y), m_vel_x(v_x), m_vel_y(v_y)
+            : m_pos{x,y}, m_vel{0,0}
         {}
         ~Dot()
         {}
+        void update_movement(const Dot &other_dot)
+        {
+            Vector2Add(m_pos, m_vel);
+        }
 
-        float m_x {static_cast<float>(GetRandomValue(0,screen_w))};
-        float m_y {static_cast<float>(GetRandomValue(0,screen_h))};
-        float m_vel_x {0};
-        float m_vel_y {0};
+        Vector2 m_pos{static_cast<float>(GetRandomValue(0,screen_h)),static_cast<float>(GetRandomValue(0,screen_w))}; //initialized on random position on map
+        Vector2 m_vel {0,0};
         float m_radius {2};
 };
 
@@ -47,16 +53,31 @@ class World
         {
             for(std::size_t i {0}; i<all_dots.size(); i++)
             {
-                DrawCircle(all_dots[i]->m_x, all_dots[i]->m_y, all_dots[i]->m_radius, Color{WHITE});
+                DrawCircle(all_dots[i]->m_pos.x, all_dots[i]->m_pos.y, all_dots[i]->m_radius, Color{WHITE});
             }
         }
     private:
         std::vector<std::shared_ptr<Dot>> all_dots {};
 };
 
+void update_player_pos(Dot &player)
+{
+    player.m_pos = Vector2Add(player.m_pos, player.m_vel);
+}
+
+void draw_player(const Dot &player)
+{
+    DrawCircle(player.m_pos.x, player.m_pos.y, player.m_radius, Color{RED});
+}
+
+//--------------------------------------------------------------------------------------------
+//MAIN FUNCTION
+//--------------------------------------------------------------------------------------------
+
 int main ()
 {
     World world;
+    Dot player {static_cast<float>(screen_w/2), static_cast<float>(screen_h/2),0,0};
     
     for(size_t i {0}; i<starting_dots; i++)
     {
@@ -84,6 +105,25 @@ int main ()
         ClearBackground(BLACK);
         BeginMode2D(camera);
 
+        if(IsKeyDown(KEY_LEFT))
+        {
+        player.m_vel.x -= player_speed;
+        }
+        if(IsKeyDown(KEY_RIGHT))
+        {
+        player.m_vel.x += player_speed;
+        }
+        if(IsKeyDown(KEY_UP))
+        {
+        player.m_vel.y -= player_speed;
+        }
+        if(IsKeyDown(KEY_DOWN))
+        {
+        player.m_vel.y += player_speed;
+        }
+
+
+
         if(IsKeyPressed(KEY_I))
         {
             std::cout << "key I pressed" << std::endl;
@@ -100,6 +140,13 @@ int main ()
             world.add_dot_on_click();
         }
         
+        draw_player(player);
+        update_player_pos(player);
+
+        //friction
+        player.m_vel.x *= friction;
+        player.m_vel.y *= friction;
+
         /*
         world.update_dots_velocity();
         world.check_boundaries();
